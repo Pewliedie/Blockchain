4dfc7a4385c67792a9c9e7769782c8310422cd0d
Whalesgohigh <56083057+Pewliedie@users.noreply.github.com>
2020-05-12 11:45:04 +0600

    Blockchain 12.05.2020

--- Blockchain/task/src/blockchain/Block.java
+++ Blockchain/task/src/blockchain/Block.java
@@ -1,32 +1,57 @@
 package blockchain;
 
 import java.util.Date;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
+
 public class Block {
-    private static final AtomicInteger UID_SUPPLIER = new AtomicInteger(0);
+    Random random = new Random();
+    private static final AtomicInteger UID_SUPPLIER = new AtomicInteger(1);
+    private volatile long minerNumber;
+    private volatile String previousHash;
+    private volatile String hash;
+    private volatile long timestamp;
+    private volatile int uid;
+    private volatile long magicNumber;
+    public volatile long miningDuration;
+    public volatile String increaseN;
 
-    private final String previousHash;
-    private final String hash;
-    private final long timestamp;
-    private final int uid;
 
-    public Block(Block previous) {
-        previousHash = previous == null? "0": previous.hash;
-        timestamp = new Date().getTime();
-        uid = UID_SUPPLIER.getAndIncrement();
+    public Block(Block previous, int zeros, long minerNumber, String increaseN) {
+        synchronized (Block.class) {
+            this.minerNumber = minerNumber;
+            this.increaseN = increaseN;
 
-        hash = StringUtil.sha256(this.toString());
+            long startTime = new Date().getTime();
+            previousHash = previous == null ? "0" : previous.hash;
+            timestamp = new Date().getTime();
+            uid = UID_SUPPLIER.getAndIncrement();
+
+            do {
+                magicNumber = random.nextInt(1000000000);
+                hash = StringUtil.sha256(this.toString());
+            } while (!hash.startsWith("0".repeat(zeros)));
+            long endTime = new Date().getTime();
+            miningDuration = TimeUnit.MILLISECONDS.toSeconds(endTime - startTime);
+        }
     }
 
     @Override
-    public String toString() {
-        return String.format("Block:%n" +
-                "Id: %d%n" +
-                "Timestamp: %d%n" +
-                "Hash of the previous block: %n" +
-                "%s%n" +
-                "Hash of the block: %n" +
-                "%s", uid, timestamp, previousHash, hash);
+    public synchronized String toString() {
+        return String.format(
+
+                "Block:%n" +
+                        "Created by miner # %d%n" +
+                        "Id: %d%n" +
+                        "Timestamp: %d%n" +
+                        "Magic number: %d%n" +
+                        "Hash of the previous block: %n" +
+                        "%s%n" +
+                        "Hash of the block: %n" +
+                        "%s%n" +
+                        "Block was generating for %d seconds %n" +
+                        "%s", minerNumber, uid, timestamp, magicNumber, previousHash, hash, miningDuration,increaseN);
     }
 }
--- Blockchain/task/src/blockchain/Main.java
+++ Blockchain/task/src/blockchain/Main.java
@@ -1,15 +1,30 @@
 package blockchain;
 
-public class Main {
-    public static void main(String[] args) {
-        int hashNumber = 5;
 
-        Block cursor = null;
-        for (int i = 0; i < hashNumber; i++) {
-            cursor = new Block(cursor);
-            System.out.println(cursor);
-            System.out.println();
-        }
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread t1 = new MiningThreads();
+        Thread t2 = new MiningThreads();
+        Thread t3 = new MiningThreads();
+        Thread t4 = new MiningThreads();
+        Thread t5 = new MiningThreads();
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t3.join();
+
+        Thread.sleep(5000);
+        BlockChain.printBlocks();
+    }
+}
+
+
+class MiningThreads extends Thread{
+    @Override
+    public void run() {
+        BlockChain.createBlock();
     }
 }
 
